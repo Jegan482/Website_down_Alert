d20bca3ebc44cd5489be2947e70679c517ba6a81
Jegan482 <jegan28122005@gmail.com>
2025-12-03 11:24:41 +0530

    Nothing

--- Website_Monitor/BACKEND/test_db.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from app.database import websites_col
-
-async def run():
-    count = await websites_col.count_documents({})
-    print("Mongo Connection Success! Websites count:", count)
-
-asyncio.run(run())
--- Website_Monitor/BACKEND/test_env.py
+++
@@ -1,6 +0,0 @@
-from dotenv import load_dotenv
-load_dotenv()
-
-import os
-print("MONGO_URI =", os.getenv("MONGO_URI"))
-print("SMTP_USER =", os.getenv("SMTP_USER"))
