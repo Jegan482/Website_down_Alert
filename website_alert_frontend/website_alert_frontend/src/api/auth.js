95afb2c70c296b9e7bac7d65ce05c85776878d98
Jegan482 <jegan28122005@gmail.com>
2026-02-24 18:55:27 +0530

    Updated frontend API to Render backend

--- website_alert_frontend/website_alert_frontend/src/api/auth.js
+++ website_alert_frontend/website_alert_frontend/src/api/auth.js
@@ -1,8 +1,9 @@
 // src/api/auth.js
 import axios from "axios";
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://website-monitor-backend.onrender.com";
 
+// ================= LOGIN =================
 export async function loginUser(username, password) {
   const resp = await axios.post(`${API_BASE_URL}/auth/login`, {
     username,
@@ -11,4 +12,19 @@ export async function loginUser(username, password) {
 
   console.log("🔁 Raw login response from backend:", resp.data);
   return resp.data; // { message, user, token }
+}
+
+// ================= REGISTER =================
+export async function registerUser(payload) {
+  const resp = await axios.post(
+    `${API_BASE_URL}/auth/create`,
+    payload,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  return resp.data;
 }
